81b6f41af8d9c3d3a70fbe0899a143707d882470
md <mihkal@dunfjeld.no>
2016-12-25 20:22:59 +0100

    Added argument to show version

--- pacsec.py
+++ pacsec.py
@@ -2,9 +2,19 @@
 
 import subprocess
 import requests
+import argparse
 
 
 URL = 'https://security.archlinux.org/'
+VERSION = '0.1.2'
+
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-V', '--version', action='version',
+                        version='%(prog)s v{}'.format(VERSION))
+    return parser.parse_args()
+
 
 def request_data(url):
     r = requests.get(url)
@@ -25,15 +35,14 @@ def parse_installed_packages():
     return pkgs
 
 
-def parse_pkg_data(pkgs, installed_packages):
+def compare_pkg_data(pkgs, installed_packages):
     # For each package in Arch security tracker, check against installed packages
     print("PACKAGE{:<11} VERSION{:<7} SEVERITY{:<2} STATUS{:<9} FIX{:<10} CVE"
           .format('', '', '', '', ''))
     for pkg in pkgs:
         for p in pkg['packages']:
-            v = installed_packages.get(p)
-
-            if v == pkg['affected']:
+            installed_version = installed_packages.get(p)
+            if installed_version == pkg['affected']:
                 print("{:<18} {:<14} {:<10} {:<12} {:<2} {} {:<8} {}".format(
                     p,
                     pkg['affected'],
@@ -46,14 +55,14 @@ def parse_pkg_data(pkgs, installed_packages):
                 ))
 
 
-def main():
+def main(argv):
     subprocess.run(['/usr/bin/pacman -Qs > /tmp/pacsec.tmp'],
                    shell=True, stdout=subprocess.PIPE)
     installed_packages = parse_installed_packages()
-    data = request_data(URL + '/json')
-    parse_pkg_data(data, installed_packages)
+    data = request_data(URL + 'json')
+    compare_pkg_data(data, installed_packages)
 
 
 if __name__ == '__main__':
-    main()
-
+    args = args()
+    main(args)
