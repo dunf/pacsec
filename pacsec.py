241bd5363dd8977c2ba6108e95bec1fbd4add70f
md <mihkal@dunfjeld.no>
2017-01-03 15:53:15 +0100

    Added summary output

--- pacsec.py
+++ pacsec.py
@@ -6,12 +6,15 @@ import argparse
 
 
 URL = 'https://security.archlinux.org/'
-VERSION = '0.1.4'
+VERSION = '0.2.0'
 
 
 def args():
     parser = argparse.ArgumentParser()
+    mu = parser.add_mutually_exclusive_group()
     parser.add_argument('-V', '--version', action='version', version='%(prog)s v{}'.format(VERSION))
+    mu.add_argument('-d', '--default', action='store_true', default=True)
+    mu.add_argument('-s', '--summary', action='store_true', help='show summary of information')
     return parser.parse_args()
 
 
@@ -19,6 +22,7 @@ def request_data(url):
     r = requests.get(url)
     data = r.json()
     return data
+
 
 
 def parse_installed_packages():
@@ -35,18 +39,27 @@ def parse_installed_packages():
 
 def compare_pkg_data(pkgs, installed_packages):
     # For each pkg in the Arch security tracker, check against installed pkgs
+    info = {'Critical': 0, 'High': 0, 'Medium': 0, 'Low': 0}
     for pkg in pkgs:
         for p in pkg['packages']:
             installed_version = installed_packages.get(p)
             if installed_version == pkg['affected'] and 'Not affected' not in pkg['status']:
-                print('PACKAGE: {:<16}{}'.format('', p))
-                print('AFFECTED VERSION: {:<7}{}'.format('', pkg['affected']))
-                print('FIX: {:<20}{}'.format('', pkg['fixed']))
-                print('STATUS: {:<17}{}'.format('', pkg['status']))
-                print('VULNERABILITY: {:<10}{}'.format('', pkg['type']))
-                print('SEVERITY: {:<15}{}'.format('', pkg['severity']))
-                print('CVE: {:<20}{}\n'.format('', ', '.join(pkg['issues'])))
-
+                if args.summary:
+                    info[pkg['severity']] += 1
+                elif args.default:
+                    print('PACKAGE: {:<16}{}'.format('', p))
+                    print('AFFECTED VERSION: {:<7}{}'.format('', pkg['affected']))
+                    print('FIX: {:<20}{}'.format('', pkg['fixed']))
+                    print('STATUS: {:<17}{}'.format('', pkg['status']))
+                    print('VULNERABILITY: {:<10}{}'.format('', pkg['type']))
+                    print('SEVERITY: {:<15}{}'.format('', pkg['severity']))
+                    print('CVE: {:<20}{}\n'.format('', ', '.join(pkg['issues'])))
+    if args.summary:
+        print('Critical severity: {:>4}'.format(info.get('Critical')))
+        print('High severity: {:>8}'.format(info.get('High')))
+        print('Medium severity: {:>6}'.format(info.get('Medium')))
+        print('Low severity: {:>9}'.format(info.get('Low')))
+        print('Vulnerable packages:  {}'.format(sum(info.values())))
 
 def main():
     subprocess.run(['/usr/bin/pacman -Qs > /tmp/pacsec.tmp'], shell=True, stdout=subprocess.PIPE)
